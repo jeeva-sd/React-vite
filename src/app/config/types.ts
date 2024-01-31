745b0e7be0ec046e57126534ac2167333ab01d00
jeeva <jeevasubash64@gmail.com>
2024-02-01 00:17:12 +0530

    config reader added.

---
+++ src/app/config/types.ts
@@ -0,0 +1,14 @@
+export interface Environment {
+    [key: string]: string | undefined;
+}
+
+export interface AppInfo {
+    version: string;
+    name: string;
+    port: number;
+    environment: string;
+}
+
+export interface AppConfig {
+    app: AppInfo;
+}
