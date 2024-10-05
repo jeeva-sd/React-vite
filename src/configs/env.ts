44977dd4c6cfcd55b36d82b40ea3a2864605c3d2
jeeva-sd <jeevasubash64@gmail.com>
2024-10-05 23:52:05 +0530

    Env configs.

--- src/configs/env.ts
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup';
-
-const envSchema = yup.object().shape({
-    VITE_SOME_KEY: yup.number(),
-    VITE_DB_PASSWORD: yup.string(),
-});
-
-export const env = envSchema.validateSync(import.meta.env, { abortEarly: false, stripUnknown: true });
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { envSchema } from './src/configs/envSchema';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    const sanitizedENV = envSchema.validateSync(process.env, { abortEarly: false, stripUnknown: true });
+
+    return ({
+        define: {
+            appConfig: sanitizedENV
+        },
+        plugins: [react()],
+    });
+});
