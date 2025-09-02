3d14c6d3ac696db3f3455c9189d8a6c6a66022ef
jeeva-sd <jeevasubash64@gmail.com>
2025-09-02 11:04:46 +0530

    Add Tailwind CSS support and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig, loadEnv } from 'vite';
 import { envSchema } from './src/configs/envSchema';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
     process.env = { ...loadEnv(mode, process.cwd()) };
@@ -13,7 +14,7 @@ export default defineConfig(({ mode }) => {
         build: {
             sourcemap: sanitizedENV.VITE_MODE === 'development',  // Disable source maps in production
         },
-        plugins: [react()],
+        plugins: [react(), tailwindcss()],
         resolve: {
             alias: {
                 '~': '/src',  // Set alias for root directory
